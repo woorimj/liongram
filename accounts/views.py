0b1cd9c20d2c12cb8a2f0685d4cbec9e7bc03d20
woorimj <forestwr06@gmail.com>
2023-05-26 03:21:53 +0900

    login, logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .forms import UserCreateForm, SignUpForm
+
+from users.models import User
 
 def signup_view(request):
     #GET요청시 HTML 응답
@@ -23,4 +27,30 @@ def signup_view(request):
         else:
             #redirect
             return redirect('accounts:signup')
+        
+def login_view(request):
+    if request.method == 'GET':
+       return render(request, 'accounts/login.html', {'form': AuthenticationForm()})
+    else:
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            #로그인 처리
+            login(request, form.user_cache)
+            return redirect('index')
+        else:
+            #로그인 실패
+            return render(request, 'accounts/login.html', {'form':form})
          
+#        username = request.POST.get('username')
+#       if username == "" or username == None:
+#          pass
+#     user = User.objects.get(username==username)
+#    if user == None:
+#       pass
+ # password = request.POST.get('password')
+
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('index')
+       
